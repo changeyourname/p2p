d9664c10a65781cac54b2990c0d98c6292993cfd
cryscan <2360537343@qq.com>
2016-03-04 06:33:31 -0600

    Format code.

--- message.cc
+++ message.cc
@@ -1,16 +1,16 @@
 #include "message.h"
 using namespace std;
 int message::send_message(int socket_fd, int header, string text) {
-  int i;
-  struct standard_msg msg;
-  memset(&msg, 0, sizeof(msg));
-  msg.msg_header = header;
-  msg.msg_text = text;
-  i = send(socket_fd, &msg, sizeof(msg), 0);
-  if (i < 0) {
-    cout << "SEND FAILED:" << i << endl;
-    return i;
-  }
-   cout << "SEND SUCCESS:" << i << endl;
-  return i;
+	int i;
+	struct standard_msg msg;
+	memset(&msg, 0, sizeof(msg));
+	msg.msg_header = header;
+	msg.msg_text = text;
+	i = send(socket_fd, &msg, sizeof(msg), 0);
+	if (i < 0) {
+		cout << "SEND FAILED:" << i << endl;
+		return i;
+	}
+	cout << "SEND SUCCESS:" << i << endl;
+	return i;
 }
